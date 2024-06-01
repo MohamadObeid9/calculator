519e79e0a49e3d70e5b99a978a648354035a299d
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-06-02 00:17:04 +0300

    created buttons for each keys

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const add         = (a,b)=> a+b;
-const subtract    = (a,b)=> a-b;
-const mutiply     = (a,b)=> a*b;
-const divide      = (a,b)=> a/b;
+const add             = (a,b)=> a+b;
+const subtract        = (a,b)=> a-b;
+const mutiply         = (a,b)=> a*b;
+const divide          = (a,b)=> a/b;
+
+const firstVariable   = 0 ;
+const secondVariavle  = 0 ;
+const operator        = null ;
+
+const operate = (operator,num1,num2)=>{
+    add(num1,num2);
+    subtract(num1,num2);
+    mutiply(num1,num2);
+    divide(num1,num2);
+};
