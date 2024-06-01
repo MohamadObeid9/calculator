c72fe1dfa9411520bd8bb584098a8124639f9a65
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-06-02 01:57:14 +0300

    did the whole css for the calculator

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const subtract        = (a,b)=> a-b;
 const mutiply         = (a,b)=> a*b;
 const divide          = (a,b)=> a/b;
 
-const firstVariable   = 0 ;
-const secondVariavle  = 0 ;
-const operator        = null ;
+let firstVariable   = 0 ;
+let secondVariavle  = 0 ;
+let operator        = null ;
 
 const operate = (operator,num1,num2)=>{
     add(num1,num2);
     subtract(num1,num2);
     mutiply(num1,num2);
     divide(num1,num2);
+};
+const appearOnDisplay = ()=>{
+  const number = document.querySelector("button.number");
+  const display = document.querySelector("div");
+  number.addEventListener("click",()=>{
+      display.appendChild(number);
+      firstVariable = parseInt(number);
+  });
 };
