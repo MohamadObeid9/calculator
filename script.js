2190b85d1bfa8e8335d87a09d2def6d91279dbcf
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-06-02 14:18:13 +0300

    make the calculator work correctly for the first time

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+// declare the basic 4 math functions
 const add             = (a,b)=> a+b;
 const subtract        = (a,b)=> a-b;
 const mutiply         = (a,b)=> a*b;
 const divide          = (a,b)=> a/b;
 
 // some variables to hold the choices of the user (two number , one operator)
-let firstVariable   = 0 ;
-let secondVariavle  = 0 ;
-let operatorUsed    = null ;
+let firstVariable   = "" ;
+let secondVariavle  = "" ;
+let operatorUsed    = "" ;
 
 // a function that can detect which operator to use
 const operate = (operator,num1,num2)=>{
-    (operator === "+") ? add(num1,num2):
-    (operator === "-") ? subtract(num1,num2):
-    (operator === "*") ? mutiply(num1,num2):
-    (operator === "/") ? divide(num1,num2):
+    (num1 === "") ? num1 = 0 : num1 = parseInt(num1);
+    (num2 === "") ? num2 = 0 : num2 = parseInt(num2);
+    (operator === "+") ? displayArea.textContent = add(num1,num2):
+    (operator === "-") ? displayArea.textContent = subtract(num1,num2):
+    (operator === "*") ? displayArea.textContent = mutiply(num1,num2):
+    (operator === "/") ? displayArea.textContent = divide(num1,num2):
     alert("pls choose an operator");
 };
 
@@ -32,6 +35,7 @@ const updateDisplay = (e)=>{
 const numbers = document.querySelectorAll(".number");
 numbers.forEach( number =>{
     number.addEventListener("click",(e)=>{
+        (operatorUsed === "") ? firstVariable += e.target.textContent : secondVariavle += e.target.textContent ;
         updateDisplay(e);
     })
 });
@@ -40,7 +44,7 @@ numbers.forEach( number =>{
 const operators = document.querySelectorAll(".operator");
 operators.forEach( operator =>{
     operator.addEventListener("click",(e)=>{
-        operatorUsed =  e.target.textContent;
+        operatorUsed =  e.target.textContent;//store the operator used in this variable
         updateDisplay(e);
     })
 });
@@ -48,16 +52,18 @@ operators.forEach( operator =>{
 // a clear function that reset the display to 0
 const  clear = ()=>{
     const clearButton = document.querySelector(".clear");
-    clearButton.addEventListener("click",()=> displayArea.textContent = 0)
+    clearButton.addEventListener("click",()=>{
+        firstVariable  = "";
+        secondVariavle = "";
+        displayArea.textContent = 0})
 };
+
+const result = ()=>{
+    const resultButton = document.querySelector(".equal");
+    resultButton.addEventListener("click",()=>{
+        operate(operatorUsed,firstVariable,secondVariavle);
+    })
+};
+
+result();
 clear();
-
-
-
-
-// const result = ()=>{
-//     const resultButton = document.querySelector(".equal");
-//     resultButton.addEventListener("click",()=>{
-//         operate()
-//     })
-// };
