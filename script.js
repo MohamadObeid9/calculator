37a4c805dca1864eda16c10b7d7a2679986e3610
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-06-02 12:43:39 +0300

    fix the operate function so it can detect which operator is used and which math function to use

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const subtract        = (a,b)=> a-b;
 const mutiply         = (a,b)=> a*b;
 const divide          = (a,b)=> a/b;
 
+// some variables to hold the choices of the user (two number , one operator)
 let firstVariable   = 0 ;
 let secondVariavle  = 0 ;
-let operator        = null ;
+let operatorUsed    = null ;
 
 // a function that can detect which operator to use
 const operate = (operator,num1,num2)=>{
-    add(num1,num2);
-    subtract(num1,num2);
-    mutiply(num1,num2);
-    divide(num1,num2);
+    (operator === "+") ? add(num1,num2):
+    (operator === "-") ? subtract(num1,num2):
+    (operator === "*") ? mutiply(num1,num2):
+    (operator === "/") ? divide(num1,num2):
+    alert("pls choose an operator");
 };
 
 // initialize the display to 0
@@ -38,6 +40,7 @@ numbers.forEach( number =>{
 const operators = document.querySelectorAll(".operator");
 operators.forEach( operator =>{
     operator.addEventListener("click",(e)=>{
+        operatorUsed =  e.target.textContent;
         updateDisplay(e);
     })
 });
