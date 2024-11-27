76716adb3ef463ba6721e44383e7f3e816b9ab97
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-11-27 14:21:27 +0200

    fix the calculator , but there is an infinity problem idk how to slove

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 // declare the basic 4 math functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
-const mutiply = (a, b) => a * b;
+const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 // some variables to hold the choices of the user (two number , one operator)
 let firstVariable = "";
-let secondVariavle = "";
+let secondVariable = "";
 let operatorUsed = "";
 
 // a function that can detect which operator to use
 const operate = (operator, num1, num2) => {
   num1 === "" ? (num1 = 0) : (num1 = Number(num1)); //replaced parseInt with Number so parseInt("1.5")=1 while number("1.5")=1.5
   num2 === "" ? (num2 = 0) : (num2 = Number(num2));
-  operator === "+"
-    ? (displayArea.textContent = `${add(num1, num2)}`)
-    : operator === "-"
-    ? (displayArea.textContent = `${subtract(num1, num2)}`)
-    : operator === "*"
-    ? (displayArea.textContent = `${mutiply(num1, num2)}`)
-    : operator === "/"
-    ? num2 === 0
-      ? alert("ERROR")
-      : (displayArea.textContent = `${divide(num1, num2)}`)
-    : alert("pls choose an operator");
+  if (operator === "+") {
+    displayArea.textContent = `${add(num1, num2)}`;
+    return add(num1, num2);
+  } else if (operator === "-") {
+    displayArea.textContent = `${subtract(num1, num2)}`;
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    displayArea.textContent = `${multiply(num1, num2)}`;
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    if ((num2 = 0)) {
+      alert("ERROR");
+    } else {
+      displayArea.textContent = `${divide(num1, num2)}`;
+      return divide(num1, num2);
+    }
+  } else {
+    alert("pls choose an operator");
+  }
 };
 
 // initialize the display to 0
@@ -45,7 +53,7 @@ numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
     operatorUsed === ""
       ? (firstVariable += e.target.textContent)
-      : (secondVariavle += e.target.textContent);
+      : (secondVariable += e.target.textContent);
     updateDisplay(e);
   });
 });
@@ -64,7 +72,7 @@ const clear = () => {
   const clearButton = document.querySelector(".clear");
   clearButton.addEventListener("click", () => {
     firstVariable = "";
-    secondVariavle = "";
+    secondVariable = "";
     operatorUsed = "";
     displayArea.textContent = 0;
   });
@@ -73,7 +81,9 @@ const clear = () => {
 const result = () => {
   const resultButton = document.querySelector(".equal");
   resultButton.addEventListener("click", () => {
-    operate(operatorUsed, firstVariable, secondVariavle);
+    const result = operate(operatorUsed, firstVariable, secondVariable);
+    firstVariable = result;
+    secondVariable = 0;
   });
 };
 
